e06310a44d275f8e1e23d5349c54cd762e8509fc
Stefan Nilsson <nilsson_stefan@yahoo.com>
2025-01-05 16:10:49 +0100

    Day 13 2024

--- day13.py
+++ day13.py
@@ -2,37 +2,58 @@ import time
 
 
 def advent13_1():
-    file = open('input13ex.txt')
+    file = open('input13.txt')
+    tokens = 0
     while True:
-        nums1 = file.readline().strip('\n').split(':').split(', ')
-        nums2 = file.readline().strip('\n').split(':').split(', ')
-        nums3 = file.readline().strip('\n').split(':').split(', ')
-    print(nums1)
+        nums1 = file.readline().strip('\n').split(':')
+        nums2 = file.readline().strip('\n').split(':')
+        nums3 = file.readline().strip('\n').split(':')
+        file.readline()
+        if len(nums1) == 1:
+            break
+        a11 = int(nums1[1].split(', ')[0].lstrip(' ')[2:])
+        a12 = int(nums2[1].split(', ')[0].lstrip(' ')[2:])
+        a21 = int(nums1[1].split(', ')[1].lstrip(' ')[2:])
+        a22 = int(nums2[1].split(', ')[1].lstrip(' ')[2:])
+        b1 = int(nums3[1].split(', ')[0].lstrip(' ')[2:])
+        b2 = int(nums3[1].split(', ')[1].lstrip(' ')[2:])
+        y = (b2 - b1*a21/a11) / (a22 - a12*a21/a11)
+        x = (b1 - a12*y) / a11
+        if abs(x - int(round(x))) < 0.01 and abs(y - int(round(y))) < 0.01 and x < 101 and y < 101:
+            tokens += 3*x + y
+
+    print('Tokens(i):', tokens)
 
 
-def advent1_2():
-    file = open('input01.txt')
-    id1 = list()
-    id2 = list()
-    for line in file:
-        nums = line.split()
-        id1.append(int(nums[0]))
-        id2.append(int(nums[1]))
+def advent13_2():
+    file = open('input13.txt')
+    tokens = 0
+    while True:
+        nums1 = file.readline().strip('\n').split(':')
+        nums2 = file.readline().strip('\n').split(':')
+        nums3 = file.readline().strip('\n').split(':')
+        file.readline()
+        if len(nums1) == 1:
+            break
+        a11 = int(nums1[1].split(', ')[0].lstrip(' ')[2:])
+        a12 = int(nums2[1].split(', ')[0].lstrip(' ')[2:])
+        a21 = int(nums1[1].split(', ')[1].lstrip(' ')[2:])
+        a22 = int(nums2[1].split(', ')[1].lstrip(' ')[2:])
+        b1 = int(nums3[1].split(', ')[0].lstrip(' ')[2:]) + 10000000000000
+        b2 = int(nums3[1].split(', ')[1].lstrip(' ')[2:]) + 10000000000000
+        y = (b2 - b1*a21/a11) / (a22 - a12*a21/a11)
+        x = (b1 - a12*y) / a11
+        if abs(x - int(round(x))) < 0.01 and abs(y - int(round(y))) < 0.01:
+            tokens += 3*x + y
 
-    sim_score = 0
-    for i in range(len(id1)):
-        for j in range(len(id2)):
-            if id2[j] == id1[i]:
-                sim_score += id1[i]
-
-    print('Similarity score:', sim_score)
+    print('Tokens(ii):', tokens)
 
 
 if __name__ == '__main__':
 
     start_time = time.time()
-    print('Advent 1')
+    print('Advent 13')
     advent13_1()
-    # advent31_2()
+    advent13_2()
     end_time_1 = time.time()
     print("time elapsed: {:.2f}s".format(end_time_1 - start_time))
